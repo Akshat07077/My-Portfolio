0f9c431c43a87ac6181c603541b5980511f8a26c
Akshat07077 <messageakshat@gmail.com>
2024-06-27 14:39:57 +0530

    Update main.js

--- static/js/main.js
+++ static/js/main.js
@@ -84,3 +84,27 @@ const showMenu = ( toggleId, navId ) => {
   
   /*SCROLL CONTACT*/
   // sr.reveal(".contact-input", { interval: 200 });
+<script>
+  /*===== MENU SHOW =====*/
+  const showMenu = (toggleId, navId) => {
+    const toggle = document.getElementById(toggleId),
+      nav = document.getElementById(navId);
+
+    if (toggle && nav) {
+      toggle.addEventListener("click", () => {
+        nav.classList.toggle("show");
+      });
+    }
+  };
+  showMenu("nav-toggle", "nav-menu");
+
+  /*===== REMOVE MENU MOBILE =====*/
+  const navLink = document.querySelectorAll(".nav-link");
+
+  function linkAction() {
+    const navMenu = document.getElementById("nav-menu");
+    navMenu.classList.remove("show");
+  }
+  navLink.forEach((n) => n.addEventListener("click", linkAction));
+</script>
+
